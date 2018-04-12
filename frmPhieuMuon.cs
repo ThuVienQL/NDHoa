e8c5e99e67c87a499f7566209b4a096103bdf64c
duchoa97 <duchoa97@gmail.com>
2018-04-12 15:36:01 +0700

    Tạo form Phiếu mượn + CSLD
    Tạo form Phiếu mượn hoàn chỉnh, kết nối với CSDL

--- frmPhieuMuon.cs
+++ frmPhieuMuon.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 namespace QLTV
 {
     public partial class frmPhieuMuon : Form
@@ -15,6 +16,8 @@ namespace QLTV
         string strConn = @"Data Source=DESKTOP-SPIMD63;Initial Catalog=QuanLyThuVien;Integrated Security=True";
         SqlConnection conn = new SqlConnection(); // khoi tao mot doi tuong ket noi
         int chon;
+        
+
         public frmPhieuMuon()
         {
             InitializeComponent();
@@ -25,6 +28,39 @@ namespace QLTV
             btnLuu.Enabled = false;
             txtPhieuMuon.Enabled = false;
         }
+        public void DataLoadPhieuMuon()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * from PhieuMuon ",conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvPhieuMuon.DataSource = dt;
 
+            
+        }
+        private void dgvNhanVien_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            dgvPhieuMuon.Rows[e.RowIndex].Cells["STT"].Value = e.RowIndex + 1;
+        }
+        private void dgvPhieuMuon_Click(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0 && e.ColumnIndex >= 0){
+                txtPhieuMuon.Text = Convert.ToString(dgvPhieuMuon.CurrentRow.Cells["MaPM"].Value);
+                cboDG.Text = Convert.ToString(dgvPhieuMuon.CurrentRow.Cells["MaDG"].Value);
+                dtNgayMuon.Text = Convert.ToString(dgvPhieuMuon.CurrentRow.Cells["NgayMuon"].Value);
+                dtNgayHen.Text = Convert.ToString(dgvPhieuMuon.CurrentRow.Cells["NgayHenTra"].Value);
+            }
+        }
+        public void loadDBComboboxMaDocGia() {
+            //conn.Open();
+            SqlCommand cmd = new SqlCommand("select MaDG from PhieuMuon",conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cboDG.DataSource = ds.Tables[0];
+            cboDG.ValueMember = "MaDG";
+            
+        }
+
+ 
     }
 }
